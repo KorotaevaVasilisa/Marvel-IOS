2f27effcc44354a48f7bc7e479715e9a22087269
Vladislav7589 <vlad_islav_7589@mail.ru>
2022-12-12 10:53:57 +0100

    Initial Commit

---
+++ marvel-vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvel-vk
+//
+//  Created by user225687 on 12/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
