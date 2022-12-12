70c988fb055ffd856909cae1b2c4489063cac913
Vladislav7589 <vlad_islav_7589@mail.ru>
2022-12-12 14:52:08 +0100

    Added Pod, .gitignore

---
+++ marvel-vk/SnappyLayout.swift
@@ -0,0 +1,12 @@
+//
+//  SnappyLayout.swift
+//  marvel-vk
+//
+//  Created by user225687 on 12/12/22.
+//
+
+import UIKit
+
+class SnappyLayout:UICollectionViewFlowLayout{
+    let activeDistance: CGFloat = 200
+}
